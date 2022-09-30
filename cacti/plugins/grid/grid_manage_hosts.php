0b446e37a05fb3637a6c2ee21c045273a00b9116
Stephanie Buser <quic_sbuser@quicinc.com>
2022-09-30 16:22:05 -0700

    Update grid_manage_hosts.php
    Issue #9
    
    Signed-off-by: Stephanie Buser <quic_sbuser@quicinc.com>

--- cacti/plugins/grid/grid_manage_hosts.php
+++ cacti/plugins/grid/grid_manage_hosts.php
@@ -1,5 +1,5 @@
 <?php
-// $Id$
+// $Id: c39f3d778a2d75d3d49f0fd3870a36e30ba01f03 $
 /*
  +-------------------------------------------------------------------------+
  | Copyright IBM Corp. 2006, 2022                                          |
@@ -57,114 +57,116 @@ function form_actions() {
 	global $config, $grid_mhosts_actions;
 
 	/* remember or default these values */
-	load_current_session_value('type', 'sess_grid_view_manage_hosts_type', '0');
-	load_current_session_value('integrated', 'sess_grid_view_manage_hosts_integrated', '-1');
+	load_current_session_value('type', 'sess_gmh_type', '0');
+	load_current_session_value('integrated', 'sess_gmh_integrated', '-1');
 
 	/* if we are to save this form, instead of display it */
 	if (isset_request_var('selected_items')) {
 		$selected_items = rtm_sanitize_unserialize_selected_items(get_request_var('selected_items'), false);
 
 		if ($selected_items != false) {
-		if (get_request_var('drp_action') == '1') { /* delete */
-			if ((isset_request_var('delete_type')) && (get_request_var('delete_type') == '2')) { // disable
-				for ($i=0;($i<count($selected_items));$i++) {
-					$vars      = explode('|', base64_decode($selected_items[$i]));
-					$clusterid = $vars[0];
-					$host      = $vars[1];
+			if (get_request_var('drp_action') == '1') { /* delete */
+				if ((isset_request_var('delete_type')) && (get_request_var('delete_type') == '2')) { // disable
+					for ($i=0;($i<count($selected_items));$i++) {
+						$vars      = explode('|', base64_decode($selected_items[$i]));
+						$clusterid = $vars[0];
+						$host      = $vars[1];
 
-					/* error checking */
-					if ($host == '' || $clusterid == '') continue;
+						/* error checking */
+						if ($host == '' || $clusterid == '') continue;
 
-					$host_id = db_fetch_cell_prepared('SELECT id
-						FROM host
-						WHERE clusterid = ?
-						AND hostname = ?',
-						array($clusterid, $host));
+						$host_id = db_fetch_cell_prepared('SELECT id
+							FROM host
+							WHERE clusterid = ?
+							AND hostname = ?',
+							array($clusterid, $host));
 
-					if (!empty($host_id)) {
-						/* disable the host */
-						db_execute_prepared('UPDATE host SET disabled="on" WHERE id = ?', array($host_id));
+						if (!empty($host_id)) {
+							/* disable the host */
+							db_execute_prepared('UPDATE host SET disabled="on" WHERE id = ?', array($host_id));
 
-						/* update poller cache */
-						db_execute_prepared('DELETE from poller_item WHERE host_id = ?', array($host_id));
-						db_execute_prepared('DELETE from poller_reindex WHERE host_id = ?', array($host_id));
+							/* update poller cache */
+							db_execute_prepared('DELETE from poller_item WHERE host_id = ?', array($host_id));
+							db_execute_prepared('DELETE from poller_reindex WHERE host_id = ?', array($host_id));
+						}
+
+						api_grid_host_remove($host, $clusterid);
 					}
+				} else if ((isset_request_var('delete_type')) && (get_request_var('delete_type') == '1')) { // delete
+					$data_sources_to_act_on = array();
+					$graphs_to_act_on       = array();
+					$devices_to_act_on      = array();
 
-					api_grid_host_remove($host, $clusterid);
-				}
-			}else if ((isset_request_var('delete_type')) && (get_request_var('delete_type') == '1')) { // delete
-				$data_sources_to_act_on = array();
-				$graphs_to_act_on       = array();
-				$devices_to_act_on      = array();
+					for ($i=0;($i<count($selected_items));$i++) {
+						$vars      = explode('|', base64_decode($selected_items[$i]));
+						$clusterid = $vars[0];
+						$host      = $vars[1];
 
-				for ($i=0;($i<count($selected_items));$i++) {
-					$vars      = explode('|', base64_decode($selected_items[$i]));
-					$clusterid = $vars[0];
-					$host      = $vars[1];
+						/* error checking */
+						if ($host == '' || $clusterid == '') continue;
 
-					/* error checking */
-					if ($host == '' || $clusterid == '') continue;
+						$host_id = db_fetch_cell_prepared('SELECT id
+							FROM host
+							WHERE clusterid = ?
+							AND hostname = ?',
+							array($clusterid, $host));
 
-					$host_id = db_fetch_cell_prepared('SELECT id
-						FROM host
-						WHERE clusterid = ?
-						AND hostname = ?',
-						array($clusterid, $host));
+						if (!empty($host_id)) {
+							$devices_to_act_on[] = $host_id;
 
-					if (!empty($host_id)) {
-						$devices_to_act_on[] = $host_id;
+							$data_sources = db_fetch_assoc_prepared('SELECT
+								data_local.id as local_data_id
+								FROM data_local
+								WHERE data_local.host_id = ?',
+								array($host_id));
 
-						$data_sources = db_fetch_assoc_prepared('SELECT
-							data_local.id as local_data_id
-							FROM data_local
-							WHERE data_local.host_id = ?',
-							array($host_id));
+							if (cacti_sizeof($data_sources)) {
+								foreach ($data_sources as $data_source) {
+									$data_sources_to_act_on[] = $data_source['local_data_id'];
+								}
+							}
 
-						if (cacti_sizeof($data_sources)) {
-							foreach ($data_sources as $data_source) {
-								$data_sources_to_act_on[] = $data_source['local_data_id'];
+							$graphs = db_fetch_assoc_prepared('SELECT
+								graph_local.id as local_graph_id
+								FROM graph_local
+								WHERE graph_local.host_id = ?',
+								array($host_id));
+
+							if (cacti_sizeof($graphs)) {
+								foreach ($graphs as $graph) {
+									$graphs_to_act_on[] = $graph['local_graph_id'];
+								}
 							}
 						}
 
-						$graphs = db_fetch_assoc_prepared('SELECT
-							graph_local.id as local_graph_id
-							FROM graph_local
-							WHERE graph_local.host_id = ?',
-							array($host_id));
-
-						if (cacti_sizeof($graphs)) {
-							foreach ($graphs as $graph) {
-								$graphs_to_act_on[] = $graph['local_graph_id'];
-							}
-						}
+						api_grid_host_remove($host, $clusterid);
 					}
 
-					api_grid_host_remove($host, $clusterid);
-				}
+					api_data_source_remove_multi($data_sources_to_act_on);
+					api_graph_remove_multi($graphs_to_act_on);
+					api_device_remove_multi($devices_to_act_on);
+				} else {
+					for ($i=0;($i<count($selected_items));$i++) {
+						$vars      = explode('|', base64_decode($selected_items[$i]));
+						$clusterid = $vars[0];
+						$host      = $vars[1];
 
-				api_data_source_remove_multi($data_sources_to_act_on);
-				api_graph_remove_multi($graphs_to_act_on);
-				api_device_remove_multi($devices_to_act_on);
-			}else{
-				for ($i=0;($i<count($selected_items));$i++) {
-					$vars      = explode('|', base64_decode($selected_items[$i]));
-					$clusterid = $vars[0];
-					$host      = $vars[1];
+						/* error checking */
+						if ($host == '' || $clusterid == '') {
+							continue;
+						}
 
-					/* error checking */
-					if ($host == '' || $clusterid == '') continue;
-
-					api_grid_host_remove($host, $clusterid);
+						api_grid_host_remove($host, $clusterid);
+					}
 				}
 			}
-		}
 		}
 
 		header('Location: grid_manage_hosts.php?header=false');
 		exit;
 	}
 
-	if(!isset($grid_mhosts_actions[get_request_var('drp_action')])) {
+	if (!isset($grid_mhosts_actions[get_request_var('drp_action')])) {
 		header('Location: grid_manage_hosts.php?header=false');
 		exit;
 	}
@@ -187,7 +189,7 @@ function form_actions() {
 						WHERE clusterid = ?
 						AND hostname = ?',
 						array($clusterid, $host));
-				}else{
+				} else {
 					$host_info = db_fetch_cell_prepared('SELECT host
 						FROM grid_hostinfo
 						WHERE clusterid = ?
@@ -229,7 +231,7 @@ function form_actions() {
 		raise_message(40);
 		header('Location: grid_manage_hosts.php?header=false');
 		exit;
-	}else{
+	} else {
 		$save_html = "<input type='submit' value='" . __('Continue') . "' alt=''>";
 	}
 
@@ -258,14 +260,14 @@ function api_grid_host_remove($host, $clusterid) {
     Manage Hosts Functions
    --------------------- */
 
-function grid_get_host_records(&$sql_where, &$sql_join, $apply_limits = TRUE, $rows, &$sql_params) {
+function grid_get_host_records(&$sql_where, &$sql_join, $apply_limits = true, $rows, &$sql_params) {
 	$sql_order = get_order_string();
 
-	if (get_request_var('integrated') != '2') {
+	if (get_request_var('integrated') != '2' || get_request_var('type') != 0) {
 		/* clusterid sql where */
 		if (get_request_var('clusterid') == '0') {
 			/* Show all items */
-		}else {
+		} else {
 			$sql_where .= (strlen($sql_where) ? ' AND ' : 'WHERE ' ) . '(grid_clusters.clusterid=?)';
 			$sql_params[] = get_request_var('clusterid');
 		}
@@ -274,13 +276,13 @@ function grid_get_host_records(&$sql_where, &$sql_join, $apply_limits = TRUE, $r
 		if (get_request_var('integrated') != '2') {
 			if (get_request_var('type') == '-1') {
 				/* Show all items */
-			}else if (get_request_var('type') == '0') {
+			} else if (get_request_var('type') == '0') {
 				$sql_where .= (strlen($sql_where) ? ' AND ' : 'WHERE ' ) . '(grid_hostinfo.isServer="1")';
-			}else if (get_request_var('type') == '1') {
+			} else if (get_request_var('type') == '1') {
 				$sql_where .= (strlen($sql_where) ? ' AND ' : 'WHERE ' ) . '(grid_hostinfo.isServer="0" AND grid_hostinfo.licFeaturesNeeded=16)';
-			}else if (get_request_var('type') == '2') {
+			} else if (get_request_var('type') == '2') {
 				$sql_where .= (strlen($sql_where) ? ' AND ' : 'WHERE ' ) . '(grid_hostinfo.isServer="0" AND grid_hostinfo.licFeaturesNeeded=512)';
-			}else{
+			} else {
 				$sql_where .= (strlen($sql_where) ? ' AND ' : 'WHERE ' ) . '(grid_hostinfo.isServer="0")';
 			}
 		}
@@ -288,10 +290,10 @@ function grid_get_host_records(&$sql_where, &$sql_join, $apply_limits = TRUE, $r
 		/* integrated sql where */
 		if (get_request_var('integrated') == '-1') {
 			/* Show all items */
-		}else if (get_request_var('integrated') == '0') {
+		} else if (get_request_var('integrated') == '0') {
 			$sql_join   = 'INNER JOIN grid_hostinfo ON grid_clusters.clusterid=grid_hostinfo.clusterid)
 				INNER JOIN host ON host.hostname=grid_hostinfo.host AND host.clusterid=grid_hostinfo.clusterid';
-		}else if (get_request_var('integrated') == '1') {
+		} else if (get_request_var('integrated') == '1') {
 			$sql_join   = 'INNER JOIN grid_hostinfo ON grid_clusters.clusterid=grid_hostinfo.clusterid)
 				LEFT JOIN host ON host.hostname=grid_hostinfo.host AND host.clusterid=grid_hostinfo.clusterid';
 			$sql_where .= (strlen($sql_where) ? ' AND ' : 'WHERE ' ) . '(host.clusterid IS NULL)';
@@ -300,17 +302,17 @@ function grid_get_host_records(&$sql_where, &$sql_join, $apply_limits = TRUE, $r
 		/* integrated sql where */
 		if (get_request_var('lastseen') == '-2') {
 			/* don't do anything */
-		}else if (get_request_var('lastseen') == '-1') {
-			$date = date('Y-m-d H:i:s', time()-1200);
+		} else if (get_request_var('lastseen') == '-1') {
+			$date = date('Y-m-d H:i:s', time()-86400);
 			$sql_where .= (strlen($sql_where) ? ' AND ' : 'WHERE ' ) . '(grid_hostinfo.last_seen>?)';
 			$sql_params[] = $date;
-		}else if (get_request_var('lastseen') == '0') {
+		} else if (get_request_var('lastseen') == '0') {
 			$date1 = date('Y-m-d H:i:s', time()-(86400*30));
 			$date2 = date('Y-m-d H:i:s', time()-(86400*7));
 			$sql_where .= (strlen($sql_where) ? ' AND ' : 'WHERE ' ) . '(grid_hostinfo.last_seen>?) AND (grid_hostinfo.last_seen<?)';
 			$sql_params[] = $date1;
 			$sql_params[] = $date2;
-		}else if (get_request_var('lastseen') == '1') {
+		} else if (get_request_var('lastseen') == '1') {
 			$date = date('Y-m-d H:i:s', time()-(86400*30));
 			$sql_where .= (strlen($sql_where) ? ' AND ' : 'WHERE ' ) . '(grid_hostinfo.last_seen<?)';
 			$sql_params[] = $date;
@@ -319,7 +321,7 @@ function grid_get_host_records(&$sql_where, &$sql_join, $apply_limits = TRUE, $r
 		/* filter sql where */
 		if (!strlen(get_request_var('filter'))) {
 			/* Show all items */
-		}else {
+		} else {
 			$sql_where .= (strlen($sql_where) ? ' AND ' : 'WHERE ' ) . '(
 				(grid_hostinfo.host LIKE ?) OR
 				(grid_hostinfo.last_seen LIKE ?) OR
@@ -336,7 +338,7 @@ function grid_get_host_records(&$sql_where, &$sql_join, $apply_limits = TRUE, $r
 		}
 
 		if (!strlen($sql_join)) {
-			$sql_join .= ' INNER JOIN grid_hostinfo ON grid_clusters.clusterid=grid_hostinfo.clusterid)';
+			$sql_join .= ' RIGHT JOIN grid_hostinfo ON grid_clusters.clusterid = grid_hostinfo.clusterid)';
 		}
 
 		$sql_query = "SELECT grid_hostinfo.*, grid_clusters.clustername
@@ -344,7 +346,7 @@ function grid_get_host_records(&$sql_where, &$sql_join, $apply_limits = TRUE, $r
 			$sql_join
 			$sql_where
 			$sql_order";
-	}else{
+	} else {
 		/* filter sql where */
 		if (get_request_var('filter') != '') {
 			$sql_where .= ' AND ((host.description LIKE ?) OR (host.hostname LIKE ?))';
@@ -372,7 +374,7 @@ function grid_get_host_records(&$sql_where, &$sql_join, $apply_limits = TRUE, $r
 		 $sql_query .= ' LIMIT ' . ($rows*(get_request_var('page')-1)) . ',' . $rows;
 	}
 
-	//echo $sql_query;
+	//print $sql_query;
 
 	return db_fetch_assoc_prepared($sql_query, $sql_params);
 }
@@ -401,7 +403,7 @@ function manageHostsFilter() {
 							<option value='1'<?php if (get_request_var('type') == '1') {?> selected<?php }?>><?php print __('Fixed Clients');?></option>
 							<option value='2'<?php if (get_request_var('type') == '2') {?> selected<?php }?>><?php print __('Float Clients');?></option>
 							<option value='0'<?php if (get_request_var('type') == '0') {?> selected<?php }?>><?php print __('Servers');?></option>
-							<?php }else{?>
+							<?php } else {?>
 							<option value='0'<?php if (get_request_var('type') == '0') {?> selected<?php }?>><?php print __('Unknown');?></option>
 							<?php }?>
 						</select>
@@ -539,13 +541,13 @@ function grid_manage_hosts() {
 
 	if (get_request_var('rows') == -1) {
 		$rows = read_grid_config_option('grid_records');
-	}elseif (get_request_var('rows') == -2) {
+	} elseif (get_request_var('rows') == -2) {
 		$rows = 99999999;
-	}else{
+	} else {
 		$rows = get_request_var('rows');
 	}
 
-	$hosts = grid_get_host_records($sql_where, $sql_join, TRUE, $rows, $sql_params);
+	$hosts = grid_get_host_records($sql_where, $sql_join, true, $rows, $sql_params);
 
 	?>
 	<script type='text/javascript'>
@@ -587,7 +589,7 @@ function grid_manage_hosts() {
 			FROM (grid_clusters
 			$sql_join
 			$sql_where";
-	}else{
+	} else {
 		$rows_query_string = "SELECT COUNT(*)
 			FROM host
 			LEFT JOIN grid_hostinfo ON grid_hostinfo.clusterid=host.clusterid AND grid_hostinfo.host=host.hostname
@@ -598,13 +600,36 @@ function grid_manage_hosts() {
 	$total_rows = db_fetch_cell_prepared($rows_query_string, $sql_params);
 
 	$display_text = array(
-		'clustername' => array(__('Cluster Name'), 'ASC'),
-		'host'        => array(__('Host Name'), 'ASC'),
-		'hostType'    => array(__('Host Type'), 'DESC'),
-		'hostModel'   => array(__('Host Model'), 'DESC'),
-		'isServer'    => array(__('Host License'), 'DESC'),
-		'first_seen'  => array(__('First Seen'), 'DESC'),
-		'last_seen'   => array(__('Last Seen'), 'ASC')
+		'clustername' => array(
+			'display' => __('Cluster Name'),
+			'sort' => 'ASC'
+		),
+		'host'        => array(
+			'display' => __('Host Name'),
+			'sort'    => 'ASC'
+		),
+		'hostType'    => array(
+			'display' => __('Host Type'),
+			'sort'    => 'DESC'
+		),
+		'hostModel'   => array(
+			'display' => __('Host Model'),
+			'sort'    => 'DESC'
+		),
+		'isServer'    => array(
+			'display' => __('Host License'),
+			'sort'    => 'DESC'
+		),
+		'first_seen'  => array(
+			'display' => __('First Seen'),
+			'align'   => 'right',
+			'sort'    => 'DESC'
+		),
+		'last_seen'   => array(
+			'display' => __('Last Seen'),
+			'align'   => 'right',
+			'sort'    => 'ASC'
+		)
 	);
 
 	/* generate page list */
@@ -619,7 +644,7 @@ function grid_manage_hosts() {
 	html_header_sort_checkbox($display_text, get_request_var('sort_column'), get_request_var('sort_direction'), false);
 
 	if (cacti_sizeof($hosts)) {
-		if(strlen(get_request_var('filter')) >0){
+		if (strlen(get_request_var('filter')) >0){
 			foreach ($hosts as $host) {
 				$id = base64_encode($host['clusterid'] . '|' . $host['host']);
 				form_alternate_row('line' . $id, false);
@@ -633,13 +658,13 @@ function grid_manage_hosts() {
 				form_selectable_cell($display_value, $id);
 				form_selectable_cell(($host['isServer'] == 1 ? 'Server' : (($host['licFeaturesNeeded'] == 16) ? __('Fixed Client') : __('Float Client'))), $id);
 				$display_value = preg_replace('/(' . preg_quote(get_request_var('filter')) . ')/i', "<span style='background-color: #F8D93D;'>\\1</span>", $host['first_seen']);
-				form_selectable_cell($display_value, $id);
+				form_selectable_cell($display_value, $id, '', 'right');
 				$display_value = preg_replace('/(' . preg_quote(get_request_var('filter')) . ')/i', "<span style='background-color: #F8D93D;'>\\1</span>", $host['last_seen']);
-				form_selectable_cell($display_value, $id);
+				form_selectable_cell($display_value, $id, '', 'right');
 				form_checkbox_cell($host['host'], $id);
 				form_end_row();
 			}
-		}else{
+		} else {
 			foreach ($hosts as $host) {
 				$id = base64_encode($host['clusterid'] . '|' . $host['host']);
 				form_alternate_row('line' . $id, false);
@@ -648,13 +673,13 @@ function grid_manage_hosts() {
 				form_selectable_cell(str_replace('UNKNOWN_AUTO_DETECT', __('Auto Detect'), $host['hostType']), $id);
 				form_selectable_cell(str_replace('UNKNOWN_AUTO_DETECT', __('Auto Detect'), $host['hostModel']), $id);
 				form_selectable_cell(($host['isServer'] == 1 ? 'Server' : (($host['licFeaturesNeeded'] == 16) ? __('Fixed Client') : __('Float Client'))), $id);
-				form_selectable_cell($host['first_seen'], $id);
-				form_selectable_cell($host['last_seen'], $id);
+				form_selectable_cell($host['first_seen'], $id, '', 'right');
+				form_selectable_cell($host['last_seen'], $id, '', 'right');
 				form_checkbox_cell($host['host'], $id);
 				form_end_row();
 			}
 		}
-	}else{
+	} else {
 		print "<tr><td colspan='8'><em>" . __('No Hosts Defined') . "</em></td></tr>";
 	}
 
@@ -668,5 +693,21 @@ function grid_manage_hosts() {
 	draw_actions_dropdown($grid_mhosts_actions);
 
 	form_end();
+
+	?>
+	<script type='text/javascript'>
+	$(function() {
+		if ($('#type').val() != 0) {
+			$('#integrated').val('-1');
+			if ($('#integrated').selectmenu('widget')) {
+				$('#integrated').prop('disabled', true);
+				$('#integrated').selectmenu('refresh').selectmenu('disable');
+			} else {
+				$('#integrated').prop('disabled', true);
+			}
+		}
+	});
+	</script>
+	<?php
 }
 
