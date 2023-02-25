4a3b878f4da6d05927434fba510b24f5208adf91
Stephanie Buser <quic_sbuser@quicinc.com>
2023-02-24 17:18:37 -0800

    Update index.php
    replacing with unencoded content
    
    Signed-off-by: Stephanie Buser <quic_sbuser@quicinc.com>

--- cacti/plugins/gridgmgmt/locales/index.php
+++ cacti/plugins/gridgmgmt/locales/index.php
@@ -1,15 +1,4 @@
-<?php //0051f
-// RTM 10.2.0-0 527086, Aug 18 2019
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
-?>
-HR+cPur0SqqFbqTZrllcRXXx9J3+fwDmJdMhNwMuMrXeu9p9Pzu1BeFVKee5wdpliTGD7KfIbVQ0
-d3bnk27ba+N/Yn2lYA+jI9pz7bsjHb3ZG6NmtXKE+Wn5jkrrl1SVvd7QJr7OoHRPxrRCSM2Tjjjn
-tGkqrfVC/35eFQC/959xfL2lRH5WQ2rFcnaGQ+DzBqcTGWc/0HBHsM7mDBbns8NEcJA3hLvWFyIG
-wLxOwO+nIICVxNQLalnrhlm1hzYLijInLwWgl1nKIbSAeAo7BFY5D/gaZhDhdIaJ8L4zAVdPm9f/
-em8EUY8pC1TxXwnPTvREco668IjyK51QrCk0VHI9OOCOrVT0gtcCIEhyI/L9jCg4E7wyVy98djwg
-LtskNCvFs5OOn1b24H7j1qjSn6gm1MNHaGg3Vkha2uRw8yhONQjGZ/MLLVBVG2BvMcsSAanv0vrz
-5C7Eys0l6eYGTah0ZczpX4fK39ds3G9nBMrYHgKk1kJGwcULStaq701byz4pign+FkIpbSThx8Xl
-d8qc1FTI8jfGsKO3ZzxjNd/HdSJg3ATKkDLbrf8IhnpghO95VBiSmgfx7ly3dux0rSA9ySzYdsLr
-2tvMGee30mHqlEy5d6RjPLxBHFShdsVvXJUXija0S+kM6o1G3ylZ6PG1Hq4JjlsH1UsaOocn+MQo
-JeSIuBENtb3PcOyG8H4mFjcfcPtawolUgxebl3kyEvmsX8gb1JRgMMdtBrgkrHxp0Gh4iRxmke4m
-pA+XAQaQLG==
+<?php
+// $Id$
+
+header("Location:../index.php");
