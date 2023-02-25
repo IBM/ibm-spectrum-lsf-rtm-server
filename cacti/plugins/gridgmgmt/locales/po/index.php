633ca1e9ab57b15fbf6e814a5a9aedcd963264bd
Stephanie Buser <quic_sbuser@quicinc.com>
2023-02-24 17:19:37 -0800

    Update index.php
    replacing with unencoded content
    
    Signed-off-by: Stephanie Buser <quic_sbuser@quicinc.com>

--- cacti/plugins/gridgmgmt/locales/po/index.php
+++ cacti/plugins/gridgmgmt/locales/po/index.php
@@ -1,15 +1,4 @@
-<?php //0051f
-// RTM 10.2.0-0 527086, Aug 18 2019
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
-?>
-HR+cP/DKbBJJesoxgky1b+WJB6oqsGFg6zIIpk1PEBt/TxP9E1v7Xt9ENj0P34jfVO9F4QhMUHtd
-KYMiRkZL5Nsng8u7Kuv8m3dabXDw+gsnsLHVzxWYFTzT59Nh2dwd0IEePxph1FitU6e6GoogMmM+
-KADo1/cBNWFYva0wyitzAZs6CJHxgBCjWL1+8//Emt8eM+4LMrfM2wlnAVWGiuD1IUSM8yYKhpMq
-sm81NpcZVADnIdb05tfNlAT8uJTtnNOqwxL3NxmSL4fN2g2iXopuXJVwf8v8PP/bK/1/TCLmYboQ
-3vY39urT2SmPjAWNyhwOV0okQCZQzhDfy8DM4XVGMz5N30mpQwI3MGtTDMrSRMJA+2VLS3gULO+Z
-dmMfX8h/EdHTPXcfxj2Om9oiTR8gar8t/Fg3eH5+mOFW7Ch8qgl8VndLbXX+WAGo526eFmpIeoLY
-W1jNQOgzyqYsYI6M4GOaUWfHRBDUq2CCrzZsGQjLdj27v2nn4pWdOutwseH9SjMbgtw5C50l5zMm
-vweZtapleaMkZE9NJxEpX0B9VUKLnVg4M/42VVI8fHNQg61VtQ+LJpWEKy/IpJrsOGMoT0ifg+2U
-aIkCA/uPrebIa8jty8pPaTwVM/Q887bGAsc5Eur5gJ5+1rjIKRBmDq+SB+qRGsqSSH2RjQzl7lh8
-bcCPS/WrehQBCbo2PCGQSCzb+gMndl2WtS8ClLxpZerDvECoDgZ4SuCVEnLJiw5D9PTNtmv+BQzM
-Au+adwohgPdk
+<?php
+// $Id$
+
+header("Location:../index.php");
